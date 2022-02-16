84c6847b8b12e61cc2f82ef46d555e403c6b6594
Matheus <matheus.pgm2000@gmail.com>
2022-02-15 22:25:43 -0300

    fix: prettie and eslint config

--- src/app/validation/id/id.js
+++ src/app/validation/id/id.js
@@ -2,7 +2,8 @@ const Joi = require('joi');
 
 module.exports = async (req, res, next) => {
   try {
-    const validate = Joi.object({id: Joi.string().min(24).max(24)});
+    const validate = Joi.object({id: Joi.string().min(24).max(24).trim().
+      pattern(/^(?=[a-f\d]{24}$)(\d+[a-f]|[a-f]+\d)/i).required()});
     const { error } = await validate.validate(req.params, { abortEarly: true });
     if (error) throw error;
     return next();
